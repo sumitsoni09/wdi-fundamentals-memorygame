0643bb825d2a4c08a321aa04fd708c48a7b52fb6
sumitsoni09 <41563314+sumitsoni09@users.noreply.github.com>
2018-08-28 02:12:37 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,39 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again");
+		alert("Sorry, try again!");
 	}
 }
 var flipCard = function (cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].suit)
+	console.log(cards[cardID].cardImage)
 if (cardsInPlay.length === 2) {
-	} else if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert('you found a match');
-} else {
-	alert('Sorry, try again');
-	}
+	checkForMatch()
+};
 }
